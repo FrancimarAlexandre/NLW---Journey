3c107683b8da03fab7447dca5064c3c509d3d378
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-07-11 17:44:02 -0300

    feat: Implementing Participants and activities controllers

--- src/controllers/link_finder.py
+++ src/controllers/link_finder.py
@@ -1,5 +1,5 @@
 class LinkFinder:
-    def __init__(self,link_repository) -> None:
+    def __init__(self, link_repository) -> None:
         self.__link_repository = link_repository
 
     def find(self, tripId):
@@ -9,16 +9,16 @@ class LinkFinder:
             formatted_links = []
             for link in links:
                 formatted_links.append({
-                    "id":link[0],
-                    "url":link[2],
-                    "title":link[3]
+                    "id": link[0],
+                    "url": link[2],
+                    "title": link[3]
                 })
             return {
-                "body":{"links":formatted_links},
-                "status_code":200
+                "body": { "links": formatted_links },
+                "status_code": 200
             }
         except Exception as exception:
             return {
-                "body":{"error":"Bad Request","message":str(exception)},
-                "status_code":400
+                "body": { "error": "Bad Request", "message": str(exception) },
+                "status_code": 400
             }
--- src/models/repositories/links_repository.py
+++ src/models/repositories/links_repository.py
@@ -1,34 +1,31 @@
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple, List
 from sqlite3 import Connection
 
 class LinksRepository:
     def __init__(self, conn: Connection) -> None:
         self.__conn = conn
-    
-    def registry_link(self,links_info: Dict):
+
+    def registry_link(self, link_infos: Dict) -> None:
         cursor = self.__conn.cursor()
         cursor.execute(
             '''
-            INSERT INTO links
-              (id,trip_id,link,title)
-            VALUES
-              (?,?,?,?)
-            ''',(
-                links_info['id'],
-                links_info['trip_id'],
-                links_info['link'],
-                links_info['title'],
+                INSERT INTO links
+                    (id, trip_id, link, title)
+                VALUES
+                    (?, ?, ?, ?)
+            ''', (
+                link_infos["id"],
+                link_infos["trip_id"],
+                link_infos["link"],
+                link_infos["title"],
             )
         )
         self.__conn.commit()
 
-    def find_links_from_trip(self,trip_id:str) -> List[Tuple]:
+    def find_links_from_trip(self, trip_id: str) -> List[Tuple]:
         cursor = self.__conn.cursor()
         cursor.execute(
-        """
-        SELECT * FROM links WHERE trip_id = ?    
-        """,(trip_id,)
+            '''SELECT * FROM links WHERE trip_id = ?''', (trip_id,)
         )
-
         links = cursor.fetchall()
         return links
